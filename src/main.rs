6cc64fcd846dedbbe045afda40ed726f6b29e428
funera1 <funeral7776@gmail.com>
2024-11-29 09:31:48 +0900

    WIP: redis-coreとやりとりするための一連の流れを実装

--- src/main.rs
+++ src/main.rs
@@ -31,13 +31,23 @@ fn set_result_size(vm: &mut Vm<Instance>, size: i32) {
     let _ = vm.run_func(Some(""), "set_result_size", params!(size)).expect("failed to set_result_size");
 }
 
-fn invoke(vm: &mut Vm<Instance>, request: i32, request_size: i32) -> Vec<WasmValue> {
-    let ret = vm.run_func(Some(""), "invoke", params!(request, request_size)).unwrap();
-    return ret;
+fn invoke(vm: &mut Vm<Instance>, request: i32, request_size: i32) {
+    let _ = vm.run_func(Some(""), "invoke", params!(request, request_size)).unwrap();
 }
 
-fn server_main() {
-    // memoryを取得
+fn server_main(vm: &mut Vm<Instance>) {
+    // 1. memory領域を確保＆ポインタを取得
+    let mut memory = allocate(vm, 10, 0)[0];
+
+    // 2. memoryにrequestを書き込む
+    let request ="PING";
+    memory = request as WasmValue;
+
+    // 3. invokeにmemoryを渡す
+    invoke(vm, memory.to_i32(), request.len() as i32);
+
+    // 4. get_result_ptrでinvokeの結果を取得
+    let response =get_result_ptr(vm)[0];
 }
 
 fn main() {
