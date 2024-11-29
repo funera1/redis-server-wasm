eb0d8e2a43fe67e717ab49d0943585171ca31cbd
funera1 <funeral7776@gmail.com>
2024-11-29 14:58:12 +0900

    動いた

--- src/lib.rs
+++ src/lib.rs
@@ -33,14 +33,14 @@ fn invoke(vm: &Vm, request: i32, request_size: i32) {
     let _ = vm.run_func(Some("redis-core"), "invoke", params!(request, request_size)).unwrap();
 }
 
-pub fn server_main(vm: &Vm) {
+pub fn query_and_response(vm: &Vm, request: &str) -> String {
     // 1. memoryを取得
     let mut memory = vm.named_module("redis-core")
                             .expect("Not found active_module")
                             .memory("memory").expect("Not found memory");
 
     // 2. memory領域を確保＆ポインタを取得
-    let request ="PING";
+    // let request ="PING";
     let req_ptr = allocate(vm, request.len() as i32, 0)[0];
 
     // 3. memoryにrequestを書き込む
@@ -52,9 +52,31 @@ pub fn server_main(vm: &Vm) {
     // 5. get_result_ptrでinvokeの結果を取得
     let response_ptr = get_result_ptr(vm)[0];
     let response_size = get_result_size(vm)[0];
-    let response =memory.read(response_ptr.to_i32() as u32, response_size.to_i32() as u32).expect("Failed to read memory");
-    println!("{}", String::from_utf8(response).unwrap());
+    let resp_bytes =memory.read(response_ptr.to_i32() as u32, response_size.to_i32() as u32).expect("Failed to read memory");
+    let response = String::from_utf8(resp_bytes).unwrap();
+    // println!("{}", String::from_utf8(response).unwrap());
 
     // 6. memory開放
     let _ = release_objects(vm);
+
+    return response;
+}
+
+pub fn init_redis_core() -> Vm {
+    let wasm_lib_file = "wasm/redis.wasm";
+
+    // create a config with the `wasi` option enabled
+    let config = ConfigBuilder::default()
+                            .build().expect("Failed to config build");
+
+    // create a VM with the config
+    let vm_base = VmBuilder::new().with_config(config)
+                        .build().expect("Failed to vmbuild");
+
+    // NOTE: active_instance以外マイグレーションできない可能性あり
+    let vm = vm_base
+        .register_module_from_file("redis-core", &wasm_lib_file)
+        .expect("Failed to register module");
+
+    return vm;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,76 @@
-use wasmedge_sdk::{VmBuilder, config::ConfigBuilder};
 mod lib;
 
+// use crate::velocity::database::DatabaseOps;
+use wasmedge_sdk::Vm;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread::spawn;
+use std::vec;
+// use velocity::query;
+
+static IP: &str = "0.0.0.0:6379";
+
+
 fn main() {
-    let wasm_lib_file = "wasm/redis.wasm";
+    // let mut db = DatabaseOps;
+    let listener = TcpListener::bind(IP);
+    let listener = match listener {
+        Ok(listener) => listener,
+        Err(e) => {
+            println!("Failed to bind to {}: {}", IP, e.kind());
+            return;
+        }
+    };
 
-    // create a config with the `wasi` option enabled
-    let config = ConfigBuilder::default()
-                            .build().expect("Failed to config build");
+    // db.delete_expired_keys();
 
-    // create a VM with the config
-    let vm_base = VmBuilder::new().with_config(config)
-                        .build().expect("Failed to vmbuild");
+    println!("Server listening on {}", IP);
 
-    // NOTE: active_instance以外マイグレーションできない可能性あり
-    let vm = vm_base
-        .register_module_from_file("redis-core", &wasm_lib_file)
-        .expect("Failed to register module");
+    let vm = lib::init_redis_core();
 
-    lib::server_main(&vm);
-    //     .run_func(Some("wasm-lib"), "fib", params!(num))?;
-    // println!("fib({}) = {}", num, res[0].to_i32());
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_commands(&stream, &vm);
+            }
+
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+
+    println!("Server shutting down");
+}
+
+fn handle_commands(mut stream: &TcpStream, vm: &Vm) {
+    let mut buffer = vec![0; 1024 * 100]; // 100kb buffer
+
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(size) if size > 0 => {
+                let data = &buffer[..size];
+                let request = String::from_utf8_lossy(data);
+                println!("{}", request);
+                let response = lib::query_and_response(vm, &request);
+                println!("{}", response);
+                // let query = query::Query::new(&response);
+                // let response = query.create_response();
+
+                stream
+                    .write_all(&response.as_bytes())
+                    .expect("Failed to write to stream");
+            }
+
+            Ok(_) => {
+                println!("Client closed the connection");
+                break;
+            }
+
+            Err(e) => {
+                println!("Error reading from stream: {}", e);
+                break;
+            }
+        }
+    }
 }
