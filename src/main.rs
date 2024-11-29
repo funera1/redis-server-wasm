e8dd677ba0c0f9bd2de0ed002d4ddae15f703235
funera1 <funeral7776@gmail.com>
2024-11-29 11:37:00 +0900

    PONGがかえってきた

--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,42 @@ use wasmedge_sdk::{params, Instance, Module, Store, Vm, WasmVal, WasmValue, VmBu
 
 fn allocate(vm: &Vm, size: i32, type_tag: i32) -> Vec<WasmValue> {
     // let params = params!(size, type_tag);
-    let ret = vm.run_func(Some(""), "allocate", params!(size, type_tag)).expect("failed to allocate");
+    let ret = vm.run_func(Some("redis-core"), "allocate", params!(size, type_tag)).expect("failed to allocate");
     return ret;
 }
 
 fn release_objects(vm: &Vm) {
-    let _ = vm.run_func(Some(""), "release_objects", params!()).expect("failed to release_objects");
+    let _ = vm.run_func(Some("redis-core"), "release_objects", params!()).expect("failed to release_objects");
 }
 
 fn get_result_ptr(vm: &Vm) -> Vec<WasmValue> {
-    let ret = vm.run_func(Some(""), "get_result_ptr", params!()).expect("failed to get_result_ptr");
+    let ret = vm.run_func(Some("redis-core"), "get_result_ptr", params!()).expect("failed to get_result_ptr");
     return ret;
 }
 
 fn get_result_size(vm: &Vm) -> Vec<WasmValue> {
-    let ret = vm.run_func(Some(""), "get_result_size", params!()).expect("failed to get_result_size");
+    let ret = vm.run_func(Some("redis-core"), "get_result_size", params!()).expect("failed to get_result_size");
     return ret;
 }
 
 fn set_result_ptr(vm: &Vm, ptr: i32) {
-    let _ = vm.run_func(Some(""), "set_result_ptr", params!(ptr)).expect("failed to set_result_ptr");
+    let _ = vm.run_func(Some("redis-core"), "set_result_ptr", params!(ptr)).expect("failed to set_result_ptr");
 }
 
 fn set_result_size(vm: &Vm, size: i32) {
-    let _ = vm.run_func(Some(""), "set_result_size", params!(size)).expect("failed to set_result_size");
+    let _ = vm.run_func(Some("redis-core"), "set_result_size", params!(size)).expect("failed to set_result_size");
 }
 
 fn invoke(vm: &Vm, request: i32, request_size: i32) {
-    let _ = vm.run_func(Some(""), "invoke", params!(request, request_size)).unwrap();
+    let _ = vm.run_func(Some("redis-core"), "invoke", params!(request, request_size)).unwrap();
 }
 
 fn server_main(vm: &Vm) {
     // 1. memoryを取得
-    let mut memory = vm.active_module().expect("Not found active_module")
-                        .memory("").expect("Not found memory");
+    let mut memory = vm.named_module("redis-core")
+                            .expect("Not found active_module")
+                            .memory("memory").expect("Not found memory");
+
     // 2. memory領域を確保＆ポインタを取得
     let request ="PING";
     let req_ptr = allocate(vm, request.len() as i32, 0)[0];
@@ -54,6 +56,9 @@ fn server_main(vm: &Vm) {
     let response_size = get_result_size(vm)[0];
     let response =memory.read(response_ptr.to_i32() as u32, response_size.to_i32() as u32).expect("Failed to read memory");
     println!("{}", String::from_utf8(response).unwrap());
+
+    // 6. memory開放
+    let _ = release_objects(vm);
 }
 
 fn main() {
@@ -67,8 +72,9 @@ fn main() {
     let vm_base = VmBuilder::new().with_config(config)
                         .build().expect("Failed to vmbuild");
 
+    // NOTE: active_instance以外マイグレーションできない可能性あり
     let vm = vm_base
-        .register_module_from_file("wasm-lib", &wasm_lib_file)
+        .register_module_from_file("redis-core", &wasm_lib_file)
         .expect("Failed to register module");
 
     server_main(&vm);
